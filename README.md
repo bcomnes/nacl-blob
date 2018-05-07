4c69d060c543c4553016ad94e4f247f5de8ac612
Bret Comnes <bcomnes@gmail.com>
2018-05-06 19:31:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# nacl-blob [![stability][0]][1]
+[![npm version][2]][3] [![build status][4]][5] [![coverage][12]][13]
+[![downloads][8]][9] [![js-standard-style][10]][11]
+
+
+## API
+
+### `.encrypt(key, nonce, blob, [opts], [cb])`
+
+Key details
+
+Nonce details
+
+Opts details
+
+cb details
+
+### `naclBlob.decrypt(key, nonce, blob, [opts], [cb])`
---
+++ index.js
@@ -0,0 +1,44 @@
+const assert = require('assert')
+const Nanobus = require('nanobus')
+
+class NACLBlob {
+  constructor(opts) {
+    this.opts = Object.assign({ 
+      chunkLength: 1024 * 1024 // 1 MB
+    }, opts)
+  }
+
+  encrypt (key, nonce, blob, mimeType, cb) {
+    const encryptedChunks = []
+    let position = 0
+    const emitter = new Nanobus()
+
+    const worker = new Worker('./encrypt-worker.js')
+
+    worker.onmessage = (ev) => {
+      switch (event.data.name) {
+
+        case 'ENCRYPT_START_OK': {
+          return postNextChunk();
+        }
+
+        case 'ENCRYPT_CHUNK_OK': {
+          encryptedChunks.push(event.data.encryptedChunks)
+          emitter.emit('progress', {})
+        }
+      }
+    }
+  }
+
+  decrypt (key, nonce, blob, mimeType, cb) {
+
+  }
+}
+
+function encrypt(key, nonce, blob, mimeType, opts, cb) {
+
+}
+
+function decrypt(key, nonce, blob, mimeType, opts, cb) {
+
+}
