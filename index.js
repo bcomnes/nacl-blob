d86c95d35dbe7d01f6f7887e407540d8e4e81f4c
Bret Comnes <bcomnes@gmail.com>
2018-05-14 19:13:11 -0700

    Checking in basic experiments

--- index.js
+++ index.js
@@ -1,26 +1,118 @@
-const assert = require('assert')
-const Nanobus = require('nanobus')
-const window = require('global/window')
+// const window = require('global/window')
+// const assert = require('assert')
+// const Nanobus = require('nanobus')
+const filereaderStream = require('filereader-stream')
+const concatStream = require('concat-stream')
+const pump = require('pump')
+const through = require('through2')
+const nacl = require('nacl-stream')
 
-let id = 0
-
-// Blob or file
+// let id = 0
 function encrypt (key, nonce, blob, opts, cb) {
-  const worker = new window.Worker('./encrypt-worker.js')
-  const bus = new Nanobus('encrypt-' + id++)
-  bus.worker = worker
+  if (!opts) opts = {}
+  if (typeof opts === 'function') {
+    cb = opts
+    opts = {}
+  }
+  opts = Object.assign(opts, {
+    chunkSize: 1024 * 1024
+  }) // defaults
 
-  worker.onmessage = (ev) => {
-    switch (ev.data.name) {
-      default: {
-        console.log(ev.data)
-        cb()
-      }
+  // const bus = new Nanobus('encrypt-' + id++)
+  const frs = filereaderStream(blob, { chunkSize: opts.chunkSize })
+  const concat = concatStream(gotEncrypted)
+  let encryptor = nacl.stream.createEncryptor(key, nonce, opts.chunkSize)
+
+  let encrypted = null
+
+  pump(frs, through(encrypt, finalizeEncrypt), concat, streamEnded)
+
+  function encrypt (chunk, enc, cb) {
+    let encryptedChunk
+    try {
+      encryptedChunk = encryptor.encryptChunk(chunk, false)
+    } catch (e) {
+      return cb(e)
     }
+    cb(null, encryptedChunk)
   }
 
-  worker.postMessage({name: 'FOO', msg: 'bar'})
-  return bus
+  function finalizeEncrypt (cb) {
+    let endChunk
+    try {
+      endChunk = encryptor.encryptChunk(new Uint8Array([]), true)
+      encryptor.clean()
+      encryptor = null
+    } catch (e) {
+      return cb(e)
+    }
+    cb(null, endChunk)
+  }
+
+  function gotEncrypted (encryptedSomething) {
+    encrypted = encryptedSomething
+  }
+
+  function streamEnded (err) {
+    if (err) return cb(err)
+    cb(null, encrypted)
+  }
+
+  // return bus
 }
 
 exports.encrypt = encrypt
+
+function decrypt (key, nonce, blob, opts, cb) {
+  if (!opts) opts = {}
+  if (typeof opts === 'function') {
+    cb = opts
+    opts = {}
+  }
+  opts = Object.assign(opts, {
+    chunkSize: 1024 * 1024
+  }) // defaults
+
+  // const bus = new Nanobus('encrypt-' + id++)
+  const frs = filereaderStream(blob, { chunkSize: opts.chunkSize })
+  const concat = concatStream(gotEncrypted)
+  let decryptor = nacl.stream.createDecryptor(key, nonce, opts.chunkSize)
+
+  let decrypted = null
+
+  pump(frs, through(decrypt, finalizeDecrypt), concat, streamEnded)
+
+  function decrypt (chunk, enc, cb) {
+    let decryptedChunk
+    try {
+      decryptedChunk = decryptor.decryptChunk(chunk, false)
+    } catch (e) {
+      return cb(e)
+    }
+    cb(null, decryptedChunk)
+  }
+
+  function finalizeDecrypt (cb) {
+    let endChunk
+    try {
+      endChunk = decryptor.decryptChunk(new Uint8Array([]), true)
+      decryptor.clean()
+      decryptor = null
+    } catch (e) {
+      return cb(e)
+    }
+    cb(null, endChunk)
+  }
+
+  function gotEncrypted (decryptedSomething) {
+    decrypted = decryptedSomething
+  }
+
+  function streamEnded (err) {
+    if (err) return cb(err)
+    cb(null, decrypted)
+  }
+
+  // return bus
+}
+exports.decrypt = decrypt
---
+++ test/index.js
@@ -0,0 +1,41 @@
+/* eslint-env browser */
+const test = require('tape')
+const naclBlob = require('../index')
+
+test('test stream', t => {
+  const key = new Uint8Array(32)
+  const nonce = new Uint8Array(16)
+  // var arr = nacl.util.decodeUTF8('Hello, chunky!');
+  const arr = new Uint8Array(10 * 1024 * 1024 + 111)
+  for (let i = 0; i < arr.length; i++) arr[i] = i & 0xff
+  const blob = new Blob([arr])
+
+  naclBlob.encrypt(key, nonce, blob, (err, encrypted) => {
+    t.error(err)
+
+    naclBlob.encrypt(key, nonce, new Blob([encrypted]), (err, decrypted) => {
+      t.error(err)
+
+      console.log(arr)
+      console.log(decrypted)
+      compareBlobs(new Blob([arr]), new Blob([decrypted]))
+      t.end()
+    })
+  })
+})
+
+function compareBlobs (a, b) {
+  var r1 = new FileReader()
+  r1.onload = function () {
+    var r2 = new FileReader()
+    r2.onload = function () {
+      if (r1.result !== r2.result) {
+        console.error('blobs differ')
+      } else {
+        console.log('blobs are equal')
+      }
+    }
+    r2.readAsBinaryString(b)
+  }
+  r1.readAsBinaryString(a)
+}
